ff37d384827d95242c61722f8cca7be93f898afb
bogdan bozic <116156416+gybo03@users.noreply.github.com>
2022-10-25 19:31:28 +0200

    gausMetodaGotova

--- GausJordan/src/CMatrix.java
+++ GausJordan/src/CMatrix.java
@@ -1,8 +1,6 @@
-import java.util.Arrays;
-
 public class CMatrix {
-    public double[][] matrix;
-    public String name;
+    private double[][] matrix;
+    private String name;
 
     public CMatrix(int x, int y, String name) {
         matrix = new double[x][y];
@@ -13,8 +11,18 @@ public class CMatrix {
         this.matrix = matrix;
         this.name = name;
     }
-
-    public Cordinates findLeadingOne(int row) {
+    //<editor-fold desc="THESE WORK">
+    public void gaus() {
+        for (int i = 0; i < matrix.length; i++) {
+            makeLeadingOneOnTop(i);
+            System.out.println(this);
+            makeLeadingOnesOne(i);
+            System.out.println(this);
+            makeNonZerosBeneathLeadingOneZeros(i);
+            System.out.println(this);
+        }
+    }
+    private Cordinates findLeadingOne(int row) {
         for (int i = 0; i < matrix[0].length; i++) {
             if (matrix[row][i] != 0) {
                 return new Cordinates(row, i);
@@ -22,28 +30,33 @@ public class CMatrix {
         }
         return new Cordinates(-1, 0);
     }
-
-    public void Gaus() {
-        for (int i = 0; i < matrix.length; i++) {
-            makeLeadingOneOnTop(i);
-            makeLeadingOnesOne(i);
-            if (i > 0 && matrix[i][0] > 0) {
-                //rowOperation();
-            }
-        }
-    }
-
-    public void rowOperation(int sourceRow, double coefficient, int destinationRow) {
+    private void rowOperation(int sourceRow, double coefficient, int destinationRow) {
         for (int i = 0; i < matrix[0].length; i++) {
             matrix[destinationRow][i] += matrix[sourceRow][i] * coefficient;
         }
     }
-    //<editor-fold desc="THESE WORK">
-    public void makeLeadingOneOnTop(int subMatrixLevel) {
+    private int findNonZerosBeneathLeadingOne(int subMatrixLevel,int coloumOfLeadingOne){
+        for (int i = subMatrixLevel+1; i < matrix.length; i++) {
+            if (matrix[i][coloumOfLeadingOne]!=0){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    private void makeNonZerosBeneathLeadingOneZeros(int subMatrixLevel){
+        while(findNonZerosBeneathLeadingOne(subMatrixLevel,findLeadingOne(subMatrixLevel).getY())!=0){
+            rowOperation(subMatrixLevel,
+                    -matrix[findNonZerosBeneathLeadingOne(subMatrixLevel,findLeadingOne(subMatrixLevel).getY())][findLeadingOne(subMatrixLevel).getY()],
+                    findNonZerosBeneathLeadingOne(subMatrixLevel,findLeadingOne(subMatrixLevel).getY())
+            );
+        }
+    }
+    private void makeLeadingOneOnTop(int subMatrixLevel) {
         switchRows(findLeftMostLeadingOne(subMatrixLevel).getY(), subMatrixLevel);
     }
 
-    public Cordinates findLeftMostLeadingOne(int subMatrixLevel) {
+    private Cordinates findLeftMostLeadingOne(int subMatrixLevel) {
         for (int i = 0; i < matrix[0].length; i++) {
             for (int j = subMatrixLevel; j < matrix.length; j++) {
                 if (matrix[j][i] != 0) {
@@ -54,16 +67,16 @@ public class CMatrix {
         return new Cordinates(-1, 0);
     }
 
-    public void makeLeadingOnesOne(int subMatrixLevel) {
-        if (matrix[subMatrixLevel][0] != 1) {
-            double coefficient = 1 / matrix[subMatrixLevel][0];
+    private void makeLeadingOnesOne(int subMatrixLevel) {
+        if (matrix[subMatrixLevel][findLeadingOne(subMatrixLevel).getY()] != 1) {
+            double coefficient = 1 / matrix[subMatrixLevel][findLeadingOne(subMatrixLevel).getY()];
             for (int i = subMatrixLevel; i < matrix[0].length; i++) {
                 matrix[subMatrixLevel][i] = matrix[subMatrixLevel][i] * coefficient;
             }
         }
     }
 
-    public void switchRows(int source, int destination) {
+    private void switchRows(int source, int destination) {
         double[] temp = matrix[source];
         matrix[source] = matrix[destination];
         matrix[destination] = temp;
