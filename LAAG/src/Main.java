7f15615bfeeb85e37f8a3b105cea25db209eab24
bogdan bozic <116156416+gybo03@users.noreply.github.com>
2022-12-03 12:14:37 +0100

    added porjecto

--- LAAG/src/Main.java
+++ LAAG/src/Main.java
@@ -2,21 +2,19 @@ import Vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        int[] iC = {0, 0, 0};
-        int[] tC1 = {1, 1, 1};
-        int[] tC2 = {0, 0, 1};
+        double[] iC = {0, 0, 0};
+        double[] tC1 = {2, -1, 3};
+        double[] tC2 = {4, -1, 2};
 
-        int[] tC3 = {1, 0};
-        int[] tC4 = {0, -1};
+        double[] tC3 = {1, 0};
+        double[] tC4 = {0, -1};
 
         Vector v = new Vector("v", iC, tC1);
         System.out.println(v);
         Vector w = new Vector("w", iC, tC2);
         System.out.println(w);
-        System.out.println(Vector.dotProduct(v, w));
-        System.out.println(v.getMagnitude() * w.getMagnitude());
-        System.out.println(Vector.angleBetweenTwoVectors(v, w));
-        System.out.println(Math.acos(Vector.dotProduct(v, w) / (v.getMagnitude() * w.getMagnitude())) * 57.2957795131);
+        Vector a= Vector.projectionOnOtherVector("a",v,w);
+        System.out.println(a);
 
         /*
         System.out.println(Math.cos(Math.PI));Vector w = Vector.multiplyVectorByScalar("-v",v,-1);
--- LAAG/src/Vector/Vector.java
+++ LAAG/src/Vector/Vector.java
@@ -4,54 +4,59 @@ public class Vector {
 
     private final String name;
     private final int nSpace;
-    private final int[] initialCoordinates;
-    private final int[] terminalCoordinates;
-    private int[] coordinatesRelativeToOrigin;
+    private final double[] initialCoordinates;
+    private final double[] terminalCoordinates;
+    private double[] coordinatesRelativeToOrigin;
     private double magnitude;
 
-    public Vector(String name, int[] initialCoordinates, int[] terminalCoordinates) {
+    public Vector(String name, double[] initialCoordinates, double[] terminalCoordinates) {
         this.name = name;
         this.initialCoordinates = initialCoordinates;
         this.terminalCoordinates = terminalCoordinates;
 
         nSpace = initialCoordinates.length;
-        coordinatesRelativeToOrigin = new int[nSpace];
+        coordinatesRelativeToOrigin = new double[nSpace];
         coordinatesRelativeToOrigin = getCoordinatesRelativeToOrigin();
         magnitude = getMagnitude();
     }
 
-    public int[] getCoordinatesRelativeToOrigin() {
+    public double[] getCoordinatesRelativeToOrigin() {
         for (int i = 0; i < nSpace; i++) {
             coordinatesRelativeToOrigin[i] = terminalCoordinates[i] - initialCoordinates[i];
         }
         return coordinatesRelativeToOrigin;
     }
 
+    public static Vector projectionOnOtherVector(String name, Vector v, Vector w) {
+        return new Vector(name,new double[v.nSpace],Vector.multiplyVectorByScalar(name,w,Vector.dotProduct(v,w)/w.getMagnitude()/w.getMagnitude()).coordinatesRelativeToOrigin);
+    }
+
     public double getMagnitude() {
-        double temp=0;
+        double temp = 0;
         for (int i = 0; i < nSpace; i++) {
             temp += Math.pow(initialCoordinates[i] + terminalCoordinates[i], 2);
         }
         return Math.sqrt(temp);
     }
-    public static Vector addVectors(String name, Vector v, Vector w){
-        int[] tC=new int[v.nSpace];
+
+    public static Vector addVectors(String name, Vector v, Vector w) {
+        double[] tC = new double[v.nSpace];
         for (int i = 0; i < v.nSpace; i++) {
-            tC[i]=v.coordinatesRelativeToOrigin[i]+w.coordinatesRelativeToOrigin[i];
+            tC[i] = v.coordinatesRelativeToOrigin[i] + w.coordinatesRelativeToOrigin[i];
         }
-        return new Vector(name,new int[v.nSpace],tC);
+        return new Vector(name, new double[v.nSpace], tC);
     }
 
-    public static Vector subtractVectors(String name, Vector v, Vector w){
-        int[] tC=new int[v.nSpace];
+    public static Vector subtractVectors(String name, Vector v, Vector w) {
+        double[] tC = new double[v.nSpace];
         for (int i = 0; i < v.nSpace; i++) {
-            tC[i]=v.coordinatesRelativeToOrigin[i]-w.coordinatesRelativeToOrigin[i];
+            tC[i] = v.coordinatesRelativeToOrigin[i] - w.coordinatesRelativeToOrigin[i];
         }
-        return new Vector(name,new int[v.nSpace],tC);
+        return new Vector(name, new double[v.nSpace], tC);
     }
 
-    public static Vector multiplyVectorByScalar(String name, Vector v, int scalar) {
-        Vector w = new Vector(name, new int[v.nSpace], new int[v.nSpace]);
+    public static Vector multiplyVectorByScalar(String name, Vector v, double scalar) {
+        Vector w = new Vector(name, new double[v.nSpace], new double[v.nSpace]);
         for (int i = 0; i < v.nSpace; i++) {
             w.terminalCoordinates[i] = v.coordinatesRelativeToOrigin[i] * scalar;
         }
@@ -60,19 +65,20 @@ public class Vector {
         return w;
     }
 
-    public static double dotProduct(Vector v,Vector w,int angle){
-        return Math.round(v.magnitude*w.magnitude*Math.cos(angle*0.0174532925));
+    public static double dotProduct(Vector v, Vector w, int angle) {
+        return Math.round(v.magnitude * w.magnitude * Math.cos(angle * 0.0174532925));
     }
-    public static double dotProduct(Vector v,Vector w){
-        double dotP=0;
+
+    public static double dotProduct(Vector v, Vector w) {
+        double dotP = 0;
         for (int i = 0; i < v.nSpace; i++) {
-            dotP+=v.coordinatesRelativeToOrigin[i]*w.coordinatesRelativeToOrigin[i];
+            dotP += v.coordinatesRelativeToOrigin[i] * w.coordinatesRelativeToOrigin[i];
         }
         return dotP;
     }
 
-    public static int angleBetweenTwoVectors(Vector v,Vector w){
-        return (int)Math.round(Math.acos(Vector.dotProduct(v,w)/(v.getMagnitude()* w.getMagnitude()))*57.2957795131);
+    public static int angleBetweenTwoVectors(Vector v, Vector w) {
+        return (int) Math.round(Math.acos(Vector.dotProduct(v, w) / (v.getMagnitude() * w.getMagnitude())) * 57.2957795131);
 
     }
 
@@ -81,7 +87,7 @@ public class Vector {
         stringBuilder.append(name + "= ");
         stringBuilder.append('(');
         for (int i = 0; i < nSpace; i++) {
-            stringBuilder.append(String.format("%2d", coordinatesRelativeToOrigin[i]));
+            stringBuilder.append(String.format("%.2f", coordinatesRelativeToOrigin[i]));
             if (i + 1 != nSpace) {
                 stringBuilder.append(",");
             }
